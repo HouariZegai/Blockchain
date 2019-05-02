f0b44ff3cd883c9ab290839606ead094f1a61f04
HouariZEGAI <houarizegai14@gmail.com>
2019-05-02 11:07:47 +0100

    Test my first blockchain (run app)

---
+++ src/com/houarizegai/blockchain/Main.java
@@ -0,0 +1,16 @@
+package com.houarizegai.blockchain;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Blockchain blockchain = new Blockchain();
+        blockchain.addBlock(new String[]{"Houari send mohamed 120 bitcoin.", "Abdelkader send 6 bitcoin to houari."});
+        blockchain.addBlock(new String[]{"Mohamed send 60 bitcoin to mom."});
+        blockchain.addBlock(new String[]{"Omar send 13 bitcoin to karim."});
+
+        System.out.println("Blocks hash:");
+        for(Block block : blockchain.getBlocks()) {
+            System.out.println(block.getHash());
+        }
+    }
+}
